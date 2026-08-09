44c67358b66a1d6dbb40d6368b807fc01317f9ca
Antart <antartconadai@gmail.com>
2026-08-08 21:00:46 -0300

    primeiro commit

---
+++ api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from API import loadjson
+
+app = FastAPI()
+
+data = loadjson.load()
+
+class upd(BaseModel):
+    item: str | int | float | list
+
+@app.get("/characters")
+def characters():
+    return data
+
+@app.get("/characters/{name}")
+def character(name: str):
+    return data[name]
+
+@app.get("/characters/{name}/{statistic}")
+def statistic(name: str, statistic: str):
+    return data[name][statistic]
+
+@app.put("/characters/{name}/{statistic}/{item}")
+def update_item(dados: upd, name: str, statistic: str, item: str):
+    if name in data and statistic in data[name] and item in data [name][statistic]:
+        data[name][statistic][item] = dados.item
+        loadjson.save(data)
+    return loadjson.load()
+
+@app.put("/characters/{name}/{statistic}")
+def update_item(dados: upd, name: str, statistic: str):
+    if name in data and statistic in data[name]:
+        data[name][statistic] = dados.item
+        loadjson.save(data)
+    return loadjson.load()
---
+++ loadjson.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+FILE = "API/bd.json"
+def load(file=FILE):
+    if not os.path.exists(FILE):
+        return {}
+    
+    with open(file, "r", encoding="utf-8") as path:
+        data = json.load(path)
+    return data
+
+def save(new_data, file=FILE):
+    with open(FILE, "w", encoding="utf-8") as path:
+        json.dump(new_data, path, ensure_ascii=False, indent=4)
+
